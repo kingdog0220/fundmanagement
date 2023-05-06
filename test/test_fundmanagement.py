51452a3267b97e7de8c5ae3c7ee403b7d07fa2b1
kingdog0220 <kingdog0220@gmail.com>
2023-05-06 15:06:29 +0900

    #25 テストコードの配置場所を変更

--- test/test_fundmanagement.py
+++ test/test_fundmanagement.py
@@ -1,17 +1,13 @@
-import json
 import os
 import shutil
 import unittest
 
 import file
-from fund.gmo import GMO
 from fund.wealthadvisor import WealthAdvisor
 
 TEST_DIR = r"D:\src\fundmanagement\test\test_case\rakuten"
 TEST_DIR_OLD = r"D:\src\fundmanagement\test\test_case\rakuten\old"
 FILE_NAME = "TotalReturn_test.csv"
-
-TEST_DIR_GMO = r"D:\src\fundmanagement\test\test_case\gmo"
 
 
 class TestFundManagement(unittest.TestCase):
@@ -43,47 +39,3 @@ class TestFundManagement(unittest.TestCase):
         wealthadvisor = WealthAdvisor()
         value = wealthadvisor.convert_to_billion("914,754百万円")
         self.assertEquals(value, 9147.54)
-
-    def test_get_gmo_account_assets(self):
-        """実際にJSONファイルを出力できるか"""
-        gmo = GMO()
-        data = gmo.get_assets()
-        dir = os.path.join(TEST_DIR_GMO, "gmo_test.json")
-        with open(dir, mode="wt", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-
-    def test_get_gmo_account_assets_jpy(self):
-        """実際にJSONファイルを出力できるか"""
-        gmo = GMO()
-        data = gmo.get_account_info("GMOJPY")
-        dir = os.path.join(TEST_DIR_GMO, "gmo_test_jpy.json")
-        with open(dir, mode="wt", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-
-    def test_get_gmo_account_assets_btc(self):
-        """実際にJSONファイルを出力できるか"""
-        gmo = GMO()
-        data = gmo.get_account_info("GMOBTC")
-        dir = os.path.join(TEST_DIR_GMO, "gmo_test_btc.json")
-        with open(dir, mode="wt", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-
-    def test_calc_amount(self):
-        gmo = GMO()
-        amount = gmo.calc_amount("120.2345", "10.12")
-        self.assertEquals(amount, 1217)
-
-    def test_calc_amount_conversionrate_exception(self):
-        gmo = GMO()
-        with self.assertRaises(ValueError):
-            gmo.calc_amount("120.2345", "ABC")
-
-    def test_calc_amount_amount_exception(self):
-        gmo = GMO()
-        with self.assertRaises(ValueError):
-            gmo.calc_amount("XYZ", "10.12")
-
-    def test_calc_amount_empty_exception(self):
-        gmo = GMO()
-        with self.assertRaises(ValueError):
-            gmo.calc_amount("", "")
