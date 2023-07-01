0390ed1014f02a594cc5c28d95e36b1dc0d9b556
kingdog0220 <kingdog0220@gmail.com>
2023-07-01 10:36:59 +0900

    #53 URLをenvファイルに移動

--- settings.py
+++ settings.py
@@ -3,18 +3,6 @@ from os.path import dirname, join
 
 from dotenv import load_dotenv
 
-# 投資信託情報
-NISSAY_TOPIX_URL = "https://www.wealthadvisor.co.jp/FundData/SnapShot.do?fnc=2015042708"
-TAWARA_DEVELOPED_COUNTRY_URL = (
-    "https://www.wealthadvisor.co.jp/FundData/SnapShot.do?fnc=2015121809"
-)
-E_MAXIS_SLIM_EMERGING_URL = (
-    "https://www.wealthadvisor.co.jp/FundData/SnapShot.do?fnc=2017073108"
-)
-E_MAXIS_SLIM_SP500_URL = (
-    "https://www.wealthadvisor.co.jp/FundData/SnapShot.do?fnc=2018070301"
-)
-
 # 各銀行・証券会社コード値
 MUFJ_BANK = "MUFJ"
 MUFJ_BANK_ACCOUNT = "MUFJ_ACC"
@@ -51,16 +39,6 @@ TOTAL_ASSET_SHEETNAME = "総資産"
 CSV_DIR = r".\fund\website\rakuten_securities"
 IMPORTED_FILE_DIR = r".\fund\website\rakuten_securities\old"
 
-# URL
-MUFJ_LOGIN_URL = (
-    "https://entry11.bk.mufg.jp/ibg/dfw/APLIN/loginib/login?_TRANID=AG004_001"
-)
-RAKUTEN_LOGIN_URL = "https://www.rakuten-sec.co.jp"
-SBI_LOGIN_URL = "https://www.sbisec.co.jp/ETGate"
-RAKUTEN_BANK_LOGIN_URL = "https://www.rakuten-bank.co.jp"
-SBI_BANK_URL = "https://www.netbk.co.jp/contents/"
-DC_LOGIN_URL = "https://dcweb.resona-tb.co.jp/web/401k/login.html"
-
 # 認証情報の読み込み
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
@@ -81,3 +59,15 @@ SBI_BANK_LOGIN_ID = os.environ.get("SBI_BANK_LOGIN_ID")
 SBI_BANK_LOGIN_PASSWORD = os.environ.get("SBI_BANK_LOGIN_PASSWORD")
 DC_LOGIN_ID = os.environ.get("DC_LOGIN_ID")
 DC_LOGIN_PASSWORD = os.environ.get("DC_LOGIN_PASSWORD")
+
+# URL
+NISSAY_TOPIX_URL = os.environ.get("NISSAY_TOPIX_URL")
+TAWARA_DEVELOPED_COUNTRY_URL = os.environ.get("TAWARA_DEVELOPED_COUNTRY_URL")
+E_MAXIS_SLIM_EMERGING_URL = os.environ.get("E_MAXIS_SLIM_EMERGING_URL")
+E_MAXIS_SLIM_SP500_URL = os.environ.get("E_MAXIS_SLIM_SP500_URL")
+MUFJ_LOGIN_URL = os.environ.get("MUFJ_LOGIN_URL")
+RAKUTEN_LOGIN_URL = os.environ.get("RAKUTEN_LOGIN_URL")
+SBI_LOGIN_URL = os.environ.get("SBI_LOGIN_URL")
+RAKUTEN_BANK_LOGIN_URL = os.environ.get("RAKUTEN_BANK_LOGIN_URL")
+SBI_BANK_URL = os.environ.get("SBI_BANK_URL")
+DC_LOGIN_URL = os.environ.get("DC_LOGIN_URL")
